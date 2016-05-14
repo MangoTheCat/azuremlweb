21e82aad54725de586e7c1ed9487923a55a49b7c
Douglas Ashton <douglas.j.ashton@gmail.com>
2016-05-14 20:42:01 +0100

    add makeRequest

---
+++ R/makeRequest.R
@@ -0,0 +1,50 @@
+
+#' Make a POST request to AzureML service
+#'
+#' @param datasets A named list of data frames to send to the web service.
+#' The names must match the names of the web inputs in Azure, eg. list(WebInput = data.frame(x=1, y=2))
+#' @param url If you supply the full URL then other options are not required
+#' @param region The Azure region, such as ussouthcentral, europewest
+#' @param workspace The workspace ID
+#' @param service The service ID
+#' @param auth Authentication token. Careful not to save this anywhere and
+#' check your .Rhistory
+#' @param details Logical. If FALSE then column types are omitted from the
+#' response
+#'
+#' @return An httr response object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' makeRequest(list(WebInput = data.frame(x=1:5, y=LETTERS[1:5])),
+#'                region = "ussouthcentral",
+#'                workspace = "8550bbba99572fc47e18e5fdd53e43e2",
+#'                service = "4e7ebd7ad816cd9dd1cb1b17c4cdb0c7",
+#'                auth = "xxxxxxxxxxxx==")
+#' }
+makeRequest <- function(datasets, url = NULL, region = "ussouthcentral",
+                        workspace=NULL, service=NULL, auth = NULL, details = TRUE) {
+  
+  if(is.null(url)) {
+    url <- buildURL(region, workspace, service, details)
+  }
+  
+  if (length(datasets) != 1) stop("Only setup for one dataset right now")
+  
+  # Build the request list. The names must match the names of the web inputs in Azure
+  req <- buildRequestList(datasets)
+  
+  if(is.null(auth)) {
+    auth <- Sys.getenv("AZUREML_APIKEY")
+    if(auth=="") {
+      stop("Either set the AZUREML_APIKEY environment variable or pass auth token")
+    }
+  }
+  auth <- paste("Bearer", auth, sep = " ")
+  
+  resp <- httr::POST(url = url, body = req, encode = "json",
+                     httr::add_headers("Authorization" = auth,
+                                       "Content-Type" = "application/json"))
+  resp
+}
