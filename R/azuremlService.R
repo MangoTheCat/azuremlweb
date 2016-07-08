8dcfa97e2f3eff0e23b7c6dedf70ff273da16e8f
Douglas Ashton <dashton@Mango.local>
2016-07-08 14:20:21 +0100

    Fixes #6 provides a better error message.

--- R/azuremlService.R
+++ R/azuremlService.R
@@ -12,7 +12,7 @@
 #' @param details Logical. If FALSE then column types are omitted from the
 #' response
 #'
-#' @return An httr response object
+#' @return A list of output data frames. If the service responds with an error code a warnring is printed and the httr::response object is returned
 #' @export
 #'
 #' @examples
@@ -30,10 +30,17 @@ azuremlService <- function(datasets, url = NULL, region = "ussouthcentral",
   resp <- makeRequest(datasets = datasets, url = url, region = region,
                       workspace = workspace, service = service, auth = auth,
                       details = details)
-  
+
   # Report errors?
-  
+  if (resp$status_code != 200) {
+    respContent <- httr::content(resp)
+    stpMsg <- paste(c("Request unsuccessful. Status code ", resp$status_code),
+                    jsonlite::toJSON(respContent$error, pretty = TRUE), collapse = "\n")
+    warning(stpMsg)
+    return(invisible(resp))
+  }
+
   # Parse the response
   parseResponse(resp)
- 
+
 }
